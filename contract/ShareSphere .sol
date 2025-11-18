01253d2f9fdea1a5de29486f6266395bcba8cbe2
Rahul Kumar Kumar <rahulk61194@gmail.com>
2025-11-18 07:27:25 -0800

    Update ShareSphere .sol

--- contract/ShareSphere .sol
+++ contract/ShareSphere .sol
@@ -48,6 +48,12 @@ struct User {
         uint256 totalShares;
         bool isRegistered;
     }
+struct User {
+        address userAddress;
+        string username;
+        uint256 totalShares;
+        bool isRegistered;
+    }
 
     
     mapping(uint256 => Content) public contents;
